43cd5fc8e25cebf6fef95b5ba1811bbd69708eda
Jarod <klionjarod@yahoo.com>
2015-10-07 11:02:06 -0400

    A test algorithm to find the GCF between 2 numbers

--- October/October5th/src/Algorithms.java
+++ October/October5th/src/Algorithms.java
@@ -2,4 +2,38 @@
  * @author Jarod Klion on 10/1/2015.
  */
 public class Algorithms {
+    public static int gcf_l(int a, int b) {
+        while (a!= b) {
+            if(a > b) {
+                a = a - b;
+            }
+            else {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+
+    public static int gcf(int a, int b) {
+        return gcf_r(a, b);
+    }
+    public static int gcf_r(int a, int b) {
+        if (a == b) {
+            return a;
+        }
+        if (a > b) {
+            return gcf_r(a - b, b);
+        }
+        else {
+            return gcf_r(a, b - a);
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println("gcf(16, 4) = "+ gcf(16,4));
+        System.out.println("gcf(1, 2) = 1 = " + gcf(1, 2));
+        System.out.println("gcf(21, 98) = 7 = " + gcf(21, 98));
+        System.out.println("gcf(1729, 13) = 13 = " + gcf(1729, 13));
+        System.out.println("gcf(1792, 4) = 1 = " + gcf(1729, 2));
+    }
 }
